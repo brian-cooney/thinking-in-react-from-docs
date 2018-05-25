8e25b18880b1f57d185b4003ec0f67a94cd3173b
brian-cooney <bcooney@gmail.com>
2018-05-25 10:15:10 +0100

    First commit, separated Dan Abramov's code pen into components to learn best pattern practices

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ProductTable from "./components/ProductTable.js";
+import SearchBar from "./components/SearchBar.js";
+
+// Filterable product table
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: "",
+      inStockOnly: false
+    };
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
